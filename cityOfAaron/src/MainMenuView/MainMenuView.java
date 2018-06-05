0598b6a9e021f0df0347b62c3eeff0098168fbe2
Giannagp <giannagp@users.noreply.github.com>
2018-06-05 14:59:19 -0600

    6/5/2018: View package

---
+++ cityOfAaron/src/MainMenuView/MainMenuView.java
@@ -0,0 +1,31 @@
+// The MainMenuView class - part of the view layer
+// Object of this class manages the main menu
+// Author: Rodriguez Hernan, Winters Karly, Echegaray Gianna team
+// Date last modified: 5 June 2018
+//-------------------------------------------------------------
+
+package MainMenuView;
+
+/**
+ *
+ * @author Gianna
+ */
+public class MainMenuView {
+  
+    // The displayMenuView method
+    // Purpose: displays the menu, gets the user's input, and does the 
+    //               selected action
+    // Parameters: none
+    // Returns: none
+    // =========================================================   
+    
+  public void displayMainMenu() {
+   
+    // Display the menu
+    // Prompt the user and get the user’s input
+    // Perform the desired action
+    // Determine and display the next view
+
+  }
+    
+}
