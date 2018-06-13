3113e0ab0ae869787f44a412c8225b654ea64893
Giannagp <giannagp@users.noreply.github.com>
2018-06-12 18:22:45 -0600

    6/5/2018: TEam Assignment code - lesson 7 - We need to run it (well)

---
+++ cityOfAaron/src/byu/cit260/view/CropView.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byu.cit260.view;
+
+/**
+ *
+ * @author Gianna
+ */
+public class CropView {
+    
+}
